09b194d5cedbf293f8bf1037bf6b46ae9b02b38e
Agata Araujo Dev <agataaraujo.dev@outlook.com>
2025-01-22 18:19:30 -0300

    add funções e variáveis

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,26 +1,38 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaParticipantes = [];
 
-function limparCampo() {
-    nome = document.querySelector('input');
-    nome.value = '';
-}
-
 function adicionarParticipante() {
     let nome = document.querySelector('input').value;
 
-    if (nome=[]){
-        alert('Por favor, insira um nome.')
+    if (nome === "") {
+        alert('Por favor, insira um nome.');
+        return;
     } else {
         listaParticipantes.push(nome);
+        document.querySelector('input').value = "";
+        atualizarLista();
+    } 
+}
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    for (let participante of listaParticipantes) {
+        lista.innerHTML += '<li>' + participante + '</li>';
     }
 }
 
 function sortear() {
+    let sorteado = document.getElementById('resultado');
+    sorteado.innerHTML = "";
+
+    if (listaParticipantes.length === 0) {
+        alert('Por favor, insira ao menos um nome.');
+        return;
+    }
+
     let indiceSorteio = parseInt(Math.random() * listaParticipantes.length + 1);
     let nomeSorteado = listaParticipantes[indiceSorteio]
 
-    prompt(`Seu amigo(a) secreto(a) é: ${nomeSorteado}`);
-    limparCampo();
+    sorteado.innerHTML = `Seu amigo(a) secreto(a) é: ${nomeSorteado}`;
 }
-
